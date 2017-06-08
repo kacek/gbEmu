dce7ba14260d36eb75a6aefb31d51b6fa7d80f16
kacek <kasper.augustyn@gmail.com>
2017-06-08 21:38:39 +0200

    Revert "Revert "implemented LD BC,d16 and LD SP,d16 opcodes. fixed 16b cartridge read/write functions""
    This reverts commit dffb3268cc9019d85e68c6a34b9039a915f3add7.

--- gbemu/z80CPU.cpp
+++ gbemu/z80CPU.cpp
@@ -23,13 +23,29 @@ void z80CPU::execute()
 		op = BUS->read8b(PC);
 		PC++;
 		switch (op) {
+		case 0x1:	//LD BC,d16 - load literal 16-bit value to register BC
+			c = BUS->read8b(PC);
+			b = BUS->read8b(PC + 1);
+			m = 3;
+			t = 12;
+			PC += 2;
+			break;
+
+		case 0x31:	//LD SP, d16 - set stack pointer to literal value
+			SP = BUS->read8b(PC);
+			m = 3;
+			t = 12;
+			PC += 2;
+			break;
 		default:
 			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 			SetConsoleTextAttribute(hConsole, 14);
-			std::cout << "Warning: Unimplemented opcode 0x" << std::hex << op  << std::endl;
+			std::cout << "Warning: Unimplemented opcode 0x" << std::hex << (int)op  << std::endl;
 			SetConsoleTextAttribute(hConsole, 15);
 			working = false;
 		}
+		clock_m += m;
+		clock_t += t;
 	}
 }
 
@@ -38,6 +54,7 @@ void z80CPU::reset()
 	a, b, c, d, e, h, l, f = 0;
 	m, t = 0;
 	PC, SP = 0;
+	clock_m, clock_t = 0;
 	working = true;
 	execute();
 }
--- gbemu/z80CPU.h
+++ gbemu/z80CPU.h
@@ -16,5 +16,6 @@ private:
 	unsigned short PC, SP;
 	bool working;
 	unsigned char op;
+	unsigned short clock_m, clock_t;
 };
 
