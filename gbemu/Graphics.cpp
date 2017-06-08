9ef410edb6de22e4f897763bf5bd14a9ba1c2024
kacek <kasper.augustyn@gmail.com>
2017-06-08 21:50:32 +0200

    fixed 16 bit read/write for PPU

--- gbemu/Graphics.cpp
+++ gbemu/Graphics.cpp
@@ -18,8 +18,8 @@ void Graphics::write8bTile(unsigned short address, unsigned char value)
 
 void Graphics::write16bTile(unsigned short address, unsigned short value)
 {
-	TileRAM[address] = (value & 0xFF00) >> 8;
-	TileRAM[address] = (value & 0x00FF);
+	TileRAM[address] = (value & 0xFF00);
+	TileRAM[address] = (value & 0x00FF) >> 8;
 }
 
 unsigned char Graphics::read8bTile(unsigned short address)
@@ -29,7 +29,7 @@ unsigned char Graphics::read8bTile(unsigned short address)
 
 unsigned short Graphics::read16bTile(unsigned short address)
 {
-	return (TileRAM[address]<<8)+TileRAM[address+1];
+	return TileRAM[address]+(TileRAM[address+1]<<8);
 }
 
 void Graphics::write8bBG1(unsigned short address, unsigned char value)
@@ -39,8 +39,8 @@ void Graphics::write8bBG1(unsigned short address, unsigned char value)
 
 void Graphics::write16bBG1(unsigned short address, unsigned short value)
 {
-	BGmap1[address] = (value & 0xFF00) >> 8;
-	BGmap1[address] = (value & 0x00FF);
+	BGmap1[address] = (value & 0xFF00);
+	BGmap1[address] = (value & 0x00FF) >> 8;
 }
 
 unsigned char Graphics::read8bBG1(unsigned short address)
@@ -50,7 +50,7 @@ unsigned char Graphics::read8bBG1(unsigned short address)
 
 unsigned short Graphics::read16bBG1(unsigned short address)
 {
-	return (BGmap1[address] << 8) + BGmap1[address + 1];
+	return BGmap1[address] + (BGmap1[address + 1] << 8);
 }
 
 void Graphics::write8bBG2(unsigned short address, unsigned char value)
@@ -60,8 +60,8 @@ void Graphics::write8bBG2(unsigned short address, unsigned char value)
 
 void Graphics::write16bBG2(unsigned short address, unsigned short value)
 {
-	BGmap2[address] = (value & 0xFF00) >> 8;
-	BGmap2[address] = (value & 0x00FF);
+	BGmap2[address] = (value & 0xFF00);
+	BGmap2[address] = (value & 0x00FF) >> 8;
 }
 
 unsigned char Graphics::read8bBG2(unsigned short address)
@@ -71,5 +71,5 @@ unsigned char Graphics::read8bBG2(unsigned short address)
 
 unsigned short Graphics::read16bBG2(unsigned short address)
 {
-	return (BGmap2[address] << 8) + BGmap2[address + 1];
+	return BGmap2[address] + (BGmap2[address + 1] << 8);
 }
