5b32406696778bd975b279e61ef6e65d5c4ae486
kacek <kasper.augustyn@gmail.com>
2017-06-08 22:20:49 +0200

    PPU memory access fix 2

--- gbemu/Graphics.cpp
+++ gbemu/Graphics.cpp
@@ -19,7 +19,7 @@ void Graphics::write8bTile(unsigned short address, unsigned char value)
 void Graphics::write16bTile(unsigned short address, unsigned short value)
 {
 	TileRAM[address] = (value & 0xFF00);
-	TileRAM[address] = (value & 0x00FF) >> 8;
+	TileRAM[address+1] = (value & 0x00FF) >> 8;
 }
 
 unsigned char Graphics::read8bTile(unsigned short address)
@@ -40,7 +40,7 @@ void Graphics::write8bBG1(unsigned short address, unsigned char value)
 void Graphics::write16bBG1(unsigned short address, unsigned short value)
 {
 	BGmap1[address] = (value & 0xFF00);
-	BGmap1[address] = (value & 0x00FF) >> 8;
+	BGmap1[address+1] = (value & 0x00FF) >> 8;
 }
 
 unsigned char Graphics::read8bBG1(unsigned short address)
@@ -61,7 +61,7 @@ void Graphics::write8bBG2(unsigned short address, unsigned char value)
 void Graphics::write16bBG2(unsigned short address, unsigned short value)
 {
 	BGmap2[address] = (value & 0xFF00);
-	BGmap2[address] = (value & 0x00FF) >> 8;
+	BGmap2[address+1] = (value & 0x00FF) >> 8;
 }
 
 unsigned char Graphics::read8bBG2(unsigned short address)
